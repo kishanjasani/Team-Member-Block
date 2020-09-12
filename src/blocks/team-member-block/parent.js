ea86d702502e7ca65f87cf77f33864a726e36483
Kishan Jasani <kishanjasani007@yahoo.in>
2020-09-12 10:21:30 +0530

    Add column attribute for the parent team member block

--- src/blocks/team-member-block/parent.js
+++ src/blocks/team-member-block/parent.js
@@ -1,6 +1,14 @@
 import { registerBlockType } from "@wordpress/blocks";
 import { __ } from "@wordpress/i18n";
-import { InnerBlocks } from "@wordpress/editor";
+import { InnerBlocks, InspectorControls } from "@wordpress/editor";
+import { PanelBody, RangeControl } from "@wordpress/components";
+
+const attributes = {
+	columns: {
+		type: "number",
+		default: 2
+	}
+};
 
 registerBlockType("team-member-block/team-members", {
 	title: __("Team Members", "team-member-block"),
@@ -12,18 +20,34 @@ registerBlockType("team-member-block/team-members", {
 		__("member", "team-member-block"),
 		__("person", "team-member-block")
 	],
-	edit: ({ className }) => {
+	attributes,
+	edit: ({ className, attributes, setAttributes }) => {
+		const { columns } = attributes;
 		return (
-			<div className={className}>
+			<div className={`${className} has-${columns}-columns`}>
+				<InspectorControls>
+					<PanelBody
+						title={__("Column Settings", "team-member-block")}
+					>
+						<RangeControl
+							label={__("Columns", "team-member-block")}
+							value={columns}
+							onChange={columns => setAttributes({ columns })}
+							min={1}
+							max={6}
+						/>
+					</PanelBody>
+				</InspectorControls>
 				<InnerBlocks
 					allowedBlocks={["team-member-block/team-member"]}
 				/>
 			</div>
 		);
 	},
-	save: () => {
+	save: ({ attributes }) => {
+		const { columns } = attributes;
 		return (
-			<div>
+			<div className={`has-${columns}-columns`}>
 				<InnerBlocks.Content />
 			</div>
 		);
